6e7ef2d7dddb92cd70e00d1b22ead6b0c8abbdfe
Edu Bonnin <edubonnin23@gmail.com>
2023-02-24 18:32:05 +0100

    Co-authored-by: denmarkrubi@gmail.com <denmarkrubi@gmail.com>

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,21 +1,27 @@
-#include "bloques.h"
 #include "ficheros_basico.h"
 
-int main(int argc, char **argv){
-    if (argc != 3){
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
         fprintf(stderr, "ERROR: Numero de parametros incorrecto. (Correcto: ./mi_mkfs <nombre_dispositivo> <nbloques>)");
-    } else {
-        //MONTAMOS EL FICHERO 
+    }
+    else
+    {
+        // MONTAMOS EL FICHERO
         bmount(argv[1]);
-        //PREPARAMOS UN BLOQUE EL CUAL ESCRIBIREMOS EN EL FICHERO HASTA LLENARLO
-        unsigned char buf [BLOCKSIZE];
-        memset(buf, '0', BLOCKSIZE);   
-        //ESCRIBIMOS EL BLOQUE HASTA LLENARLO 
+        // PREPARAMOS UN BLOQUE EL CUAL ESCRIBIREMOS EN EL FICHERO HASTA LLENARLO
+        unsigned char buf[BLOCKSIZE];
+        memset(buf, '0', BLOCKSIZE);
+        // ESCRIBIMOS EL BLOQUE HASTA LLENARLO
         for (int i = 0; i < atoi(argv[2]); i++)
         {
             bwrite(i, buf);
         }
-        //DESMONTAMOS EL FICHERO 
+        initSB(argv[2], atoi(argv[2])/ 4);
+        initMB();
+        initAI();
+        // DESMONTAMOS EL FICHERO
         bumount();
     }
 }
